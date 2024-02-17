624889cf41688282c23723f1489e630a3b9c3dbd
Soviet.Dog <soviet.dog1945@proton.me>
2024-02-17 19:32:15 +0200

    delete unused imports

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-# router = DefaultRouter()
-# router.register(r'user', base_views.FollowersViewSet)
 
 urlpatterns = [path('admin/', admin.site.urls),
                path('', include('baseapp.urls'))
